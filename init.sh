e47b1f6976517d1f9c362bb3a8bc4413bb01ee4e
Renato Mintz <remintz@gmail.com>
2019-01-14 15:39:39 -0200

    Disabled prompt

--- init.sh
+++ init.sh
@@ -29,7 +29,7 @@ apt-get install -y \
 echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/sources.list
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys F76221572C52609D
 apt-get update
-apt-get install docker-engine
+apt-get install -y docker-engine
 usermod -aG docker admin
 
 # install docker-composer
